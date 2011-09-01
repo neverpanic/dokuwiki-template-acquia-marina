781f540b2f36ddca4e083f11b288c7dca5598ce9
Clemens Lang <neverpanic@gmail.com>
2011-09-01 19:42:05 +0200

    Added headers and login form
    This is probably broken markup at the moment and will stay for a while

--- main.php
+++ main.php
@@ -1,37 +1,94 @@
 <?php
 /**
- * DokuWiki Starter Template
+ * DokuWiki port of the Acquia-Marina Drupal Template
  *
- * @link   http://dokuwiki.org/template:starter
- * @author Anika Henke <anika@selfthinker.org>
+ * @link   http://drupal.org/project/acquia_marina
+ * @author Clemens Lang (port to DokuWiki) <clemens@neverpanic.de>
  */
 
 if (!defined('DOKU_INC')) die(); /* must be run from within DokuWiki */
 @require_once(dirname(__FILE__).'/tpl_functions.php'); /* include hook for template functions */
 
-$showTools = !tpl_getConf('hideTools') || ( tpl_getConf('hideTools') && $_SERVER['REMOTE_USER'] );
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang'] ?>"
-  lang="<?php echo $conf['lang'] ?>" dir="<?php echo $lang['direction'] ?>">
+$showTools = !tpl_getConf('hideTools') || (tpl_getConf('hideTools') && $_SERVER['REMOTE_USER']);
+
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $conf['lang']; ?>" xml:lang="<?php print $conf['lang']; ?>">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title><?php tpl_pagetitle() ?> [<?php echo strip_tags($conf['title']) ?>]</title>
-    <?php tpl_metaheaders() ?>
-    <link rel="shortcut icon" href="<?php echo _tpl_getFavicon() /* DW versions > 2010-11-12 can use the core function tpl_getFavicon() */ ?>" />
-    <?php _tpl_include('meta.html') ?>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title><?php tpl_pagetitle() ?> | <?php echo strip_tags($conf['title']) ?></title>
+	<?php tpl_metaheaders() ?>
+	<link rel="shortcut icon" href="<?php echo _tpl_getFavicon() /* DW versions > 2010-11-12 can use the core function tpl_getFavicon() */ ?>" />
+	<?php _tpl_include('meta.html') ?>
 </head>
 
-<body>
-    <?php /* with these Conditional Comments you can better address IE issues in CSS files,
-             precede CSS rules by #IE6 for IE6, #IE7 for IE7 and #IE8 for IE8 (div closes at the bottom) */ ?>
-    <!--[if IE 6 ]><div id="IE6"><![endif]--><!--[if IE 7 ]><div id="IE7"><![endif]--><!--[if IE 8 ]><div id="IE8"><![endif]-->
+<?php // TODO: Add conditional logged-in or not-logged-in ?>
+<body id="pid-wiki" class="front page-node no-sidebars layout-main sidebars-both-last font-size-14 grid-type-fluid grid-width-16 fluid-90">
+	<?php /* classes mode_<action> are added to make it possible to e.g. style a page differently if it's in edit mode,
+		see http://www.dokuwiki.org/devel:action_modes for a list of action modes */ ?>
+	<?php /* .dokuwiki should always be in one of the surrounding elements (e.g. plugins and templates depend on it) */ ?>
+	<div id="page" class="page">
+		<div id="page-inner" class="page-inner">
+			<div id="skip">
+				<a href="#main-content-area">Zum eigentlichen Inhaltsbereich springen</a>
+			</div>
+		</div>
 
-    <?php /* classes mode_<action> are added to make it possible to e.g. style a page differently if it's in edit mode,
-         see http://www.dokuwiki.org/devel:action_modes for a list of action modes */ ?>
-    <?php /* .dokuwiki should always be in one of the surrounding elements (e.g. plugins and templates depend on it) */ ?>
-    <div id="dokuwiki__site"><div class="dokuwiki site mode_<?php echo $ACT ?>">
-        <?php html_msgarea() /* occasional error and info messages on top of the page */ ?>
+		<!-- The login-box -->
+		<div class="header-top-wrapper full-width" id="header-top-wrapper">
+			<div class="header-top row grid16-16" id="header-top">
+				<div class="header-top-inner inner clearfix" id="header-top-inner">
+					<div class="block block-user odd first last fusion-horiz-login grid16-16" id="block-user-0">
+						<div class="inner">
+							<div class="corner-top"><div class="corner-top-right corner"></div><div class="corner-top-left corner"></div></div>
+							<div class="inner-wrapper">
+								<div class="inner-inner">
+									<div class="block-icon pngfix"></div>
+									<h2 class="title block-title">Benutzeranmeldung</h2>
+									<div class="content clearfix">
+										<form id="user-login-form" method="post" accept-charset="UTF-8" action="/node?destination=node">
+											<input type="hidden" name="sectok" value="some-tbd-value FIXME FIXME FIXME" />
+											<input type="hidden" name="id" value="title of the current page I guess FIXME FIXME FIXME" />
+											<input type="hidden" name="do" value="login" />
+											<input type="hidden" name="r" value="0" /> <!-- Stay logged in -->
+											<div>
+												<div id="edit-name-wrapper" class="form-item overlabel-wrapper">
+													<label for="edit-name" class="overlabel-apply" style="text-indent: 0px; cursor: text;">Benutzername: <span title="Diese Angabe wird benötigt." class="form-required">*</span></label>
+													<input type="text" class="form-text required" value="" size="15" id="edit-name" name="u" maxlength="60">
+												</div>
+												<div id="edit-pass-wrapper" class="form-item overlabel-wrapper">
+													<label for="edit-pass" class="overlabel-apply" style="text-indent: 0px; cursor: text;">Passwort: <span title="Diese Angabe wird benötigt." class="form-required">*</span></label>
+													<input type="password" class="form-text required" size="15" maxlength="60" id="edit-pass" name="p">
+												</div>
+												<input type="submit" class="form-submit" value="Anmelden" id="edit-submit" name="op">
+												<div class="item-list">
+													<ul>
+														<li class="first">
+															<a title="Ein neues Benutzerkonto erstellen." href="/user/register">Registrieren</a>
+														</li>
+														<li class="last">
+															<a title="Ein neues Passwort per E-Mail anfordern." href="/user/password">Neues Passwort anfordern</a>
+														</li>
+													</ul>
+												</div>
+											</div>
+										</form>
+									</div>
+								</div><!-- /inner-inner -->
+							</div><!-- /inner-wrapper -->
+						<div class="corner-bottom"><div class="corner-bottom-right corner"></div><div class="corner-bottom-left corner"></div></div>
+					</div><!-- /inner -->
+				<div><!-- /block -->
+			</div><!-- /header-top-inner -->
+		</div><!-- /header-top -->
+	</div>
+
+		<div id="dokuwiki__site">
+			<div class="dokuwiki site mode_<?php echo $ACT ?>">
+				<div id="header-top-wrapper" class="header-top-wrapper full-width">
+					<div id="header-top" class="header-top row grid16-16">
+						<div id="header-top-inner" class="header-top-inner inner clearfix">
+							
+			<?php html_msgarea() /* occasional error and info messages on top of the page */ ?>
         <?php _tpl_include('header.html') ?>
 
         <!-- ********** HEADER ********** -->
