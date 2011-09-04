69bbe6605a53e346cc26cc4463e0a4a49c871b1f
Clemens Lang <neverpanic@gmail.com>
2011-09-04 15:52:20 +0200

    Move strings to language files

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -5,14 +5,16 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
 
-$lang['discussion']        = 'Diskussion';
-$lang['back_to_article']   = 'Zurück zum Artikel';
-$lang['userpage']          = 'Benutzer-Seite';
+$lang['discussion']            = 'Diskussion';
+$lang['back_to_article']       = 'Zurück zum Artikel';
+$lang['userpage']              = 'Benutzer-Seite';
+$lang['back_to_website']       = 'zur Website';
+$lang['back_to_website_title'] = 'zurück zur Website';
 
 /* accessibility headlines */
-$lang['user_tools']        = 'Benutzer-Werkzeuge';
-$lang['site_tools']        = 'Webseiten-Werkzeuge';
-$lang['page_tools']        = 'Seiten-Werkzeuge';
-$lang['skip_to_content']   = 'springe zum Inhalt';
+$lang['user_tools']            = 'Benutzer-Werkzeuge';
+$lang['site_tools']            = 'Webseiten-Werkzeuge';
+$lang['page_tools']            = 'Seiten-Werkzeuge';
+$lang['skip_to_content']       = 'springe zum Inhalt';
 
 //Setup VIM: ex: et ts=2 :
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -4,12 +4,14 @@
  *
  */
 
-$lang['discussion']        = 'Discussion';
-$lang['back_to_article']   = 'Back to article';
-$lang['userpage']          = 'User page';
+$lang['discussion']            = 'Discussion';
+$lang['back_to_article']       = 'Back to article';
+$lang['userpage']              = 'User page';
+$lang['back_to_website']       = 'Website';
+$lang['back_to_website_title'] = 'back to the website';
 
 /* accessibility headlines */
-$lang['user_tools']        = 'User Tools';
-$lang['site_tools']        = 'Site Tools';
-$lang['page_tools']        = 'Page Tools';
-$lang['skip_to_content']   = 'skip to content';
+$lang['user_tools']            = 'User Tools';
+$lang['site_tools']            = 'Site Tools';
+$lang['page_tools']            = 'Page Tools';
+$lang['skip_to_content']       = 'skip to content';
